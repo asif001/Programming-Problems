d95d11a74163695c3ba293725fc1150283d322b7
Asifur  Rahman <AsifurARahman@gmail.com>
2022-08-20 23:54:46 +0600

    Create two-sum.cpp

---
+++ two-sum.cpp
@@ -0,0 +1,24 @@
+//Problem: https://leetcode.com/problems/two-sum/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> idxToNumber;
+        vector<int> res(2, -1);
+        
+        for(int i = 0;i < (int)nums.size();i++){
+            int numToFind = target - nums[i];
+            
+            auto it = idxToNumber.find(numToFind);
+            
+            if(it == idxToNumber.end()){
+                idxToNumber[nums[i]] = i;
+            }else if(idxToNumber[numToFind] != i){
+                res[0] = i;
+                res[1] = idxToNumber[numToFind];
+            }
+        }
+        
+        return res;
+    }
+};
