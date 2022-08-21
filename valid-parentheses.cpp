e2ca6e445576928b4ecf788ff8b2d667db06d255
Asifur  Rahman <AsifurARahman@gmail.com>
2022-08-22 00:09:45 +0600

    Create valid-parentheses.cpp

---
+++ valid-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> brackets;
+        
+        for(int i = 0; i < (int)s.size();i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                brackets.push(s[i]);
+            }else if(s[i] == ')' || s[i] == '}' || s[i] == ']'){
+                if(brackets.empty()){
+                    return false;
+                }
+                
+                char curr = brackets.top();
+                if(curr != s[i] - 1 && curr != s[i] - 2){
+                    return false;
+                }
+                
+                brackets.pop();
+            }
+        }
+        
+        if(!brackets.empty()){
+            return false;
+        }
+        
+        return true;
+    }
+};
