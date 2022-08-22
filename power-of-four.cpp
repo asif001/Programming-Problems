33f04eda623991d155fed00ec13c27fccf94deec
Asifur  Rahman <AsifurARahman@gmail.com>
2022-08-23 00:02:25 +0600

    Update power-of-four.cpp

--- power-of-four.cpp
+++ power-of-four.cpp
@@ -17,3 +17,12 @@ public:
         }
     }
 };
+
+// Solution by bit manipulation
+
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        return (n > 0) && ((n & (n - 1)) == 0) && ((n - 1) % 3 == 0);
+    }
+};
