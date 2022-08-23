9dd580a664fdc674c5018f21407012430c9497c5
Asifur  Rahman <AsifurARahman@gmail.com>
2022-08-23 14:00:42 +0600

    Create remove-duplicates-from-sorted-array.cpp

---
+++ remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,32 @@
+// Problem: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int k = 1;
+        
+        for(int i = 0;i < (int)nums.size();i++){
+            int currNumber = nums[i];
+            int nextDiffNumIndex = i+1;
+            
+            while(nextDiffNumIndex < (int)nums.size()){
+                if(currNumber != nums[nextDiffNumIndex]){
+                    break;
+                }
+                
+                nextDiffNumIndex++;
+            }
+            
+            if(nextDiffNumIndex == (int)nums.size()){
+                return k;
+            }else{
+                nums[k] = nums[nextDiffNumIndex];
+                k++;
+            }
+            
+            i = nextDiffNumIndex-1;
+        }
+            
+        return k;
+    }
+};
