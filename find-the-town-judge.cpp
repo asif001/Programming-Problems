5fd8f4ea41f0864b1ea2e01e8358f927c3873bf2
Asifur  Rahman <AsifurARahman@gmail.com>
2022-08-25 13:25:56 +0600

    Update find-the-town-judge.cpp

--- find-the-town-judge.cpp
+++ find-the-town-judge.cpp
@@ -20,3 +20,25 @@ public:
         return -1;
     }
 };
+
+// Optimized space
+
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> numOfDegree(n+1, 0);
+        
+        for(int i = 0;i < (int)trust.size();i++){
+            numOfDegree[trust[i][0]]--;
+            numOfDegree[trust[i][1]]++;
+        }
+        
+        for(int i = 1;i <= n;i++){
+            if(numOfDegree[i] == n-1){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
