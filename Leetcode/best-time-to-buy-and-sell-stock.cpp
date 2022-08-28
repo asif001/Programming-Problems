11539fa78edb99d7000ad0d980bd5a98c6f63e17
Asifur  Rahman <AsifurARahman@gmail.com>
2022-08-29 04:33:54 +0600

    Update best-time-to-buy-and-sell-stock.cpp

--- Leetcode/best-time-to-buy-and-sell-stock.cpp
+++ Leetcode/best-time-to-buy-and-sell-stock.cpp
@@ -15,3 +15,20 @@ public:
         return profit > 0 ? profit : 0;
     }
 };
+
+// Improved by removing dp array and calculate minimum in variable
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minimum = INT_MAX;
+        int profit = 0;
+        
+        for(int i = 0;i < prices.size();i++){
+            minimum = min(prices[i], minimum);
+            profit = max(profit, prices[i] - minimum);
+        }
+        
+        return profit;
+    }
+};
