d90d2e125e6e1ebe0ff46dcf1b4c2984de169e9a
Asifur  Rahman <AsifurARahman@gmail.com>
2022-09-16 00:25:01 +0600

    Create move-zeroes.cpp

---
+++ Leetcode/move-zeroes.cpp
@@ -0,0 +1,19 @@
+// Problem: https://leetcode.com/problems/move-zeroes/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0;
+        int j = 0;
+        
+        while(j < nums.size()){
+            if(nums[j] == 0){
+                j++;
+            }else{
+                swap(nums[i], nums[j]);
+                i++;
+                j++;
+            }
+        }
+    }
+};
