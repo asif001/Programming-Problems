3af21f13686235abbf7275e5ca33a26b5125dc7f
Asifur  Rahman <AsifurARahman@gmail.com>
2022-09-18 02:05:53 +0600

    Create path-sum.cpp

---
+++ Leetcode/path-sum.cpp
@@ -0,0 +1,48 @@
+// Problem: https://leetcode.com/problems/path-sum/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSumRecursive(TreeNode* root, int targetSum){
+        if(root->left == nullptr && root->right == nullptr){
+            return targetSum == root->val;
+        }
+        
+        bool resLeft = false;
+        bool resRight = false;
+        
+        if(root->left != nullptr){
+            resLeft = hasPathSumRecursive(root->left, targetSum-root->val);
+        } 
+        
+        if(root->right != nullptr){
+            resRight = hasPathSumRecursive(root->right, targetSum-root->val);
+        }
+        
+        return resLeft || resRight;
+    }
+    
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root == nullptr){
+            return false;
+        }
+                
+        if(root->left == nullptr && root->right != nullptr){
+            return hasPathSumRecursive(root->right, targetSum-root->val);
+        }else if(root->right == nullptr && root->left != nullptr){
+            return hasPathSumRecursive(root->left, targetSum-root->val);
+        }
+        
+        return hasPathSumRecursive(root, targetSum);
+    }
+};
