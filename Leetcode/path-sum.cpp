89e3360e98a787d07941743e48b559f10ae07615
Asifur  Rahman <AsifurARahman@gmail.com>
2022-09-18 02:15:42 +0600

    Update path-sum.cpp

--- Leetcode/path-sum.cpp
+++ Leetcode/path-sum.cpp
@@ -13,30 +13,27 @@
  */
 class Solution {
 public:
-    bool hasPathSumRecursive(TreeNode* root, int targetSum){
-        if(root->left == nullptr && root->right == nullptr){
-            return targetSum == root->val;
-        }
-        
-        bool resLeft = false;
-        bool resRight = false;
-        
-        if(root->left != nullptr){
-            resLeft = hasPathSumRecursive(root->left, targetSum-root->val);
-        } 
-        
-        if(root->right != nullptr){
-            resRight = hasPathSumRecursive(root->right, targetSum-root->val);
-        }
-        
-        return resLeft || resRight;
-    }
-    
     bool hasPathSum(TreeNode* root, int targetSum) {
         if(root == nullptr){
             return false;
         }
         
-        return hasPathSumRecursive(root, targetSum);
+        if(root->left == nullptr && root->right == nullptr){
+            return targetSum == root->val;
+        }
+        
+        if(root->left != nullptr){
+            if(hasPathSum(root->left, targetSum-root->val)){
+                return true;
+            }
+        } 
+        
+        if(root->right != nullptr){
+            if(hasPathSum(root->right, targetSum-root->val)){
+                return true;
+            }
+        }
+        
+        return false;
     }
 };
