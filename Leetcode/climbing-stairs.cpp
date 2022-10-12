a32c27df31679e0f9cf0c9196083e447fd2b0f04
Asifur  Rahman <AsifurARahman@gmail.com>
2022-10-12 14:25:21 +0600

    Update climbing-stairs.cpp

--- Leetcode/climbing-stairs.cpp
+++ Leetcode/climbing-stairs.cpp
@@ -8,11 +8,11 @@ public:
     
     int climbStairs(int n) {
         if(n == 0){
-            return 1;
+            return dp[0] = 1;
         }
         
         if(n == 1){
-            return 1;
+            return dp[1] = 1;
         }
         
         if(dp[n] != 0){
